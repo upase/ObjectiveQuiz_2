931e803d346618694353a6463d1a62521759b056
upase <upase16058@iiitd.ac.in>
2016-08-17 19:56:27 +0530

    Trial commit

--- app/src/main/java/in/ac/iiitd/psingh/mc16/objectivequiz/ActivitySplash.java
+++ app/src/main/java/in/ac/iiitd/psingh/mc16/objectivequiz/ActivitySplash.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
 
 public class ActivitySplash extends AppCompatActivity {
 
@@ -11,6 +12,7 @@ public class ActivitySplash extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_activity_splash);
+
 
         new Handler().postDelayed(new Runnable() {
             @Override
